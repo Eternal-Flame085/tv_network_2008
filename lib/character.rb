bed6cea775118e1f792f352ac18b02d6c4863b5e
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-01 11:11:46 -0400

    finished character class

--- lib/character.rb
+++ lib/character.rb
@@ -1,5 +1,8 @@
 class Character
-  def initialize
-  
+  attr_reader :name, :actor, :salary
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @actor = info_hash[:actor]
+    @salary = info_hash[:salary]
   end
 end
--- test/character_test.rb
+++ test/character_test.rb
@@ -4,9 +4,16 @@ require "./lib/character"
 
 class CharacterTest < Minitest::Test
   def test_it_exists
-    character = Character.new
+    character = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
     assert_instance_of Character, character
   end
 
+  def test_it_has_values
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT", kitt.name
+    assert_equal "William Daniels", kitt.actor
+    assert_equal 1_000_000, kitt.salary
+  end
 end
